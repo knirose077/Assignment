ecd747e41f45550685932b9bc6a6e04f817943f4
Nirose k <111358060+knirose077@users.noreply.github.com>
2022-09-13 12:22:11 +0530

    Update Sec B Attri.cs

--- Sep12_Demo/Sec B Attri.cs
+++ Sep12_Demo/Sec B Attri.cs
@@ -1,1 +1,25 @@
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sep12_B
+{
+        [System.AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+        sealed class DeveloperAttribute : Attribute
+        {
+
+            public DeveloperAttribute(string songname)
+            {
+
+                this.SongName = songname;
+
+            }
+            public string SongName { get; set; }
+            public string Language { get; set; }
+            public string Lyrics { get; set; }
+        }
+    }
+
